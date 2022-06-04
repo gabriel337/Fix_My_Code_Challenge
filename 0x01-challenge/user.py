68322f32bc49e381d79cf032f4769ed5dc6ebc63
gabriel337 <g.david.lopezruiz@gmail.com>
2022-06-04 02:22:30 +0000

    add task 2

---
+++ 0x01-challenge/user.py
@@ -0,0 +1,31 @@
+!/usr/bin/python3
+"""
+User class
+"""
+
+
+class User():
+    """ Create an object named User """
+
+    def __init__(self):
+        """ initialize User object attributes """
+        self.__email = None
+
+    @property
+    def email(self):
+        """getter for private instance attribute email"""
+        return self.__email
+
+    @email.setter
+    def email(self, value):
+        """ setter for User's email """
+        if type(value) is not str:
+            raise TypeError("email must be a string")
+        self.__email = value
+
+
+if __name__ == "__main__":
+
+    u = User()
+    u.email = "john@snow.com"
+    print(u.email)
